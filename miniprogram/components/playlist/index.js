bacccb81a1f45ddadd25f19ee60f8f514d793067
xing-6464 <xing_6464@foxmail.com>
2023-02-12 21:29:41 +0800

    播放数量细节处理

--- miniprogram/components/playlist/index.js
+++ miniprogram/components/playlist/index.js
@@ -5,12 +5,42 @@ Component({
       type: Object,
     },
   },
-  data: {},
+  observers: {
+    ['playlist.playCount'](count) {
+      this.setData({
+        _count: this._tranNumber(count, 2),
+      })
+    },
+  },
+  data: {
+    _count: 0,
+  },
   lifetimes: {
     created() {},
     attached() {},
     moved() {},
     detached() {},
   },
-  methods: {},
-});
+  methods: {
+    _tranNumber(num, point) {
+      let numStr = num.toString().split('.')[0]
+      if (numStr.length < 6) {
+        return numStr
+      } else if (numStr.length >= 6 && numStr.length <= 8) {
+        let decimal = numStr.substring(
+          numStr.length - 4,
+          numStr.length - 4 + point
+        )
+
+        return parseInt(num / 1000) + '.' + decimal + '万'
+      } else if (numStr > 8) {
+        let decimal = numStr.substring(
+          numStr.length - 8,
+          numStr.length - 8 + point
+        )
+
+        return parseFloat(parseInt(num / 100000000) + '.' + decimal) + '亿'
+      }
+    },
+  },
+})
