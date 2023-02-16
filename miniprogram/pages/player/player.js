b98ceb588a3e8ad99d5374d599cb86f6a05ba5d5
xing-6464 <xing_6464@foxmail.com>
2023-02-16 23:59:28 +0800

    音乐控制面板功能实现（1）

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -2,16 +2,20 @@ let musiclist
 // 正在播放歌曲的index
 let nowPlayingIndx
 
+// 获取全局唯一的背景音频播放器
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+
 Page({
   data: {
     picUrl: '',
+    isPlayer: false, // false表示不播放，true表示正在播放
   },
   onLoad(options) {
     nowPlayingIndx = options.index
     musiclist = wx.getStorageSync('musiclist')
-    this._loadMusicDetail()
+    this._loadMusicDetail(options.musicId)
   },
-  _loadMusicDetail() {
+  _loadMusicDetail(musicId) {
     let music = musiclist[nowPlayingIndx]
 
     wx.setNavigationBarTitle({
@@ -20,6 +24,33 @@ Page({
 
     this.setData({
       picUrl: music.al.picUrl,
+      isPlayer: false,
     })
+
+    wx.showLoading({
+      title: '歌曲加载中',
+    })
+    wx.cloud
+      .callFunction({
+        name: 'music',
+        data: {
+          musicId,
+          $url: 'musicUrl',
+        },
+      })
+      .then((res) => {
+        let result = res.result.data[0]
+        backgroundAudioManager.src = result.url
+        backgroundAudioManager.title = music.name
+        backgroundAudioManager.coverImgUrl = music.al.picUrl
+        backgroundAudioManager.singer = music.ar[0].name
+        backgroundAudioManager.epname = music.al.name
+
+        this.setData({
+          isPlayer: true,
+        })
+
+        wx.hideLoading()
+      })
   },
 })
