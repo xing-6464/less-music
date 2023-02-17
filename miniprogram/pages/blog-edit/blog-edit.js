69ae4ba531040b739942c1ae740ce00f7e4b0b27
xing-6464 <xing_6464@foxmail.com>
2023-02-17 23:48:56 +0800

    选择图片业务逻辑

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -1,10 +1,14 @@
 // 输入文字最大个数
 const MAX_WORDS_NUM = 140
+// 最大上传图片数量
+const MAX_IMG_NUM = 9
 Page({
   data: {
     // 输入文字个数
     wordsNum: 0,
     footerBottom: 0,
+    images: [],
+    selectPhoto: true,
   },
   onLoad(options) {
     console.log(options)
@@ -26,6 +30,42 @@ Page({
   onBlur() {
     this.setData({
       footerBottom: 0,
+    })
+  },
+  onChooseImage() {
+    // 还能再选几张图片
+    let max = MAX_IMG_NUM - this.data.images.length
+    wx.chooseImage({
+      count: max,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        this.setData({
+          images: this.data.images.concat(res.tempFilePaths),
+        })
+        // 还能选几张图片
+        max = MAX_IMG_NUM - this.data.images.length
+        this.setData({
+          selectPhoto: max <= 0 ? false : true,
+        })
+      },
+    })
+  },
+  onDelImage(event) {
+    this.data.images.splice(event.target.dataset.index, 1)
+    this.setData({
+      images: this.data.images,
+    })
+    if (this.data.images.length === MAX_IMG_NUM - 1) {
+      this.setData({
+        selectPhoto: true,
+      })
+    }
+  },
+  onPreviewImage(event) {
+    wx.previewImage({
+      urls: this.data.images,
+      current: event.target.dataset.imgSrc,
     })
   },
   onShareAppMessage() {
