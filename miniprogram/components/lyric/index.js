0bd96a82e5208beaeb2295ec3a6d4825e5e28baa
xing-6464 <xing_6464@foxmail.com>
2023-02-17 13:51:47 +0800

    歌词数据解析

--- miniprogram/components/lyric/index.js
+++ miniprogram/components/lyric/index.js
@@ -9,15 +9,42 @@ Component({
   },
   observers: {
     lyric(lrc) {
-      console.log(lrc)
+      this._parseLyric(lrc)
     },
   },
-  data: {},
+  data: {
+    lyricList: [],
+  },
   lifetimes: {
     created() {},
     attached() {},
     moved() {},
     detached() {},
   },
-  methods: {},
+  methods: {
+    _parseLyric(sLyric) {
+      let line = sLyric.split('\n')
+      const _lrcList = []
+      line.forEach((elem) => {
+        const time = elem.match(/\[(\d{2,}):(\d{2})(?:\.(\d{2,3}))?]/g)
+        if (time != null) {
+          const lrc = elem.split(time)[1]
+          const timeReg = time[0].match(/(\d{2,}):(\d{2})(?:\.(\d{2,3}))?/)
+          // 把时间转换成秒
+          const time2Seconds =
+            parseInt(timeReg[1]) * 60 +
+            parseInt(timeReg[2]) +
+            parseInt(timeReg[3]) / 1000
+
+          _lrcList.push({
+            lrc,
+            time: time2Seconds,
+          })
+        }
+      })
+      this.setData({
+        lyricList: _lrcList,
+      })
+    },
+  },
 })
